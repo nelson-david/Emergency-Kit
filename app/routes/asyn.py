aeb418b5d7c8ed1f9df35be31aae3c08fa78d270
nelson-david <larva.code15@gmail.com>
2020-08-22 12:00:12 -0700

    all

--- app/routes/asyn.py
+++ app/routes/asyn.py
@@ -1,19 +1,19 @@
-from app import app
+from app import app, db
 from flask import redirect, url_for, request, jsonify
 from flask_login import current_user
 from app.models import Incidence, Safe
 
-@app.route("/mark_safe")
+@app.route("/mark_safe", methods=["POST"])
 def mark_safe():
-	incidence = Incidence.query.filter_by(id=request.form['id']).first()
+	incidence = Incidence.query.filter_by(id=request.form['incidence_id']).first()
 	current_user.like(incidence)
 	db.session.commit()
 
 	return jsonify({'success':'true'})
 
-@app.route("/mark_unsafe")
+@app.route("/mark_unsafe", methods=["POST"])
 def mark_unsafe():
-	incidence = Incidence.query.filter_by(id=request.form['id']).first()
+	incidence = Incidence.query.filter_by(id=request.form['incidence_id']).first()
 	current_user.unlike(incidence)
 	db.session.commit()
 
